e5d1af042fe9cacd6a48c6357455b15abe50d949
youlethal <78674543+youlethal@users.noreply.github.com>
2021-02-20 15:32:46 +0000

    Update PrisonGUI.lua

--- PrisonGUI.lua
+++ PrisonGUI.lua
@@ -863,7 +863,7 @@ GiveGuns.MouseButton1Click: Connect(function()
 	for i,v in pairs(Workspace.Prison_ITEMS.giver:GetChildren()) do
 
 OwO = Workspace.Remote.ItemHandler:InvokeServer(v.ITEMPICKUP)
-end
+
 end)
 
 
